7daec2fd28fc789b305e004b798c2d90e9bb61e9
DESKTOP-EIM2NH1\tai <choanhmotlan056@gmail.com>
2021-08-02 15:01:09 +0700

    ABC

---
+++ Shop.Model/Models/PostCategory.cs
@@ -0,0 +1,36 @@
+﻿using Shop.Model.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Model.Models
+{
+    [Table("PostCategories")]
+    public abstract class PostCategory: Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string Name { get; set; }
+        [Required]
+        [Column(TypeName ="varchar")]
+        [MaxLength(256)]
+        public string Alias { get; set; }
+        public int? ParentID { get; set; }
+        [MaxLength(256)]
+        public string Image { get; set; }
+        public int? DisplayOrder { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+        public bool? HotFlag { get; set; }
+        public virtual IEnumerable<Post> Posts { get; set; }
+        
+    }
+}
---
+++ Shop.Model/Models/ProductTag.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Model.Models
+{
+    [Table("ProductTags")]
+    public class ProductTag
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [Column(TypeName = "varchar")]
+        [MaxLength(50)]
+        [Required]
+        public string TagID { get; set; }
+        [ForeignKey("ProductID")]
+        public virtual Product Product { get; set; }
+        [ForeignKey("TagID")]
+        public virtual Tag Tag { get; set; }
+    }
+}
